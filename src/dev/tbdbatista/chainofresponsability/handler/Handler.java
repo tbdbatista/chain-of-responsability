04d8d3d9643f15837f4af914760aa35b183cdf4a
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-05-16 19:56:51 -0300

    Create Handler interface

--- src/dev/tbdbatista/chainofresponsability/handler/Handler.java
+++ src/dev/tbdbatista/chainofresponsability/handler/Handler.java
@@ -1,5 +1,7 @@
 package dev.tbdbatista.chainofresponsability.handler;
 
+import dev.tbdbatista.chainofresponsability.model.File;
+
 public interface Handler {
     public void setHandler(Handler handler);
     public void process(File file);
